5d1fc943a485faf20aa38b42a69b1c5a6df47b72
William Park <williampark@hotmail.co.uk>
2016-12-06 13:21:38 +0000

    Start of quiz functionality

---
+++ public/js/quiz.js
@@ -0,0 +1,94 @@
+function Quiz(name, questions){
+  this.quizLength = questions.length;
+  this.quizPosition = 0;
+  this.currentQuestion = questions[this.quizPosition].question;
+  this.currentAnswers = questions[this.quizPosition].answers;
+  this.currentCorrect = questions[this.quizPosition].correct;
+
+  this.refreshQuiz = function() {
+    this.currentQuestion = questions[this.quizPosition].question;
+    this.currentAnswers = questions[this.quizPosition].answers;
+    this.currentCorrect = questions[this.quizPosition].correct;
+  }
+
+  this.nextQuestion = function(){
+    if(this.quizPosition != this.quizLength-1) {
+      this.quizPosition++;
+    }
+  }
+
+  this.prevQuestion = function() {
+    if(this.quizPosition != 0) {
+      this.quizPosition --;
+    }
+  }
+};
+
+//Quiz 1:
+var q1_q1 = {question: "What element is part of H2O?",
+  answers: [
+    "Calcium",
+    "Oxygen", // Correct
+    "Lithium"
+  ],
+  correct: "Oxygen"
+};
+
+var q1_q2 = {question: "How many different elements are there?",
+  answers: [
+    "50",
+    "77",
+    "100" // Correct
+  ],
+  correct: "100"
+};
+
+var q1_questions = [q1_q1, q1_q2];
+var quiz1 = new Quiz("elements", q1_questions);
+
+
+
+
+//QUIZ FRONT END
+
+
+
+var f_reloadQuiz = function(quiz) {
+  quiz.refreshQuiz();
+  var sel_title = "#question";
+  var sel_quizList = "#quiz-form ul";
+
+  //Remove old answers
+  $(".answer").remove();
+
+  //Change the question.
+  $(sel_title).text(quiz.currentQuestion);
+
+  //Add new answers.
+  quiz1.currentAnswers.forEach(function(answer) {
+    $(sel_quizList).prepend("<li class='answer'> \
+                        <input type='radio' id='" + answer + "' name ='quiz' value='" + answer + "'/> \
+                        <label for='" + answer + "'>" + answer + "</label>\
+                      </li>"
+    );
+  });
+};
+
+
+$(function() {
+	//Check JQuery is Running
+	console.log("Quiz Running.");
+  f_reloadQuiz(quiz1);
+
+  //Next 2 lines just move on to next line.
+  quiz1.nextQuestion();
+  f_reloadQuiz(quiz1);
+
+
+  $('#quiz-form').on('submit', function(e) {
+
+    console.log("Quiz Submitted");
+    e.preventDefault();
+  });
+
+});
